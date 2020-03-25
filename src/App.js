62ac74292c7a3efbf023705b56172946c43f6f6b
ditasmer <ditasmer@gmail.com>
2020-03-25 11:43:11 +0000

    Lesson 20: alert msg on dev tools/components/state

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
 /*  async componentDidMount() {
@@ -46,6 +47,11 @@ class App extends Component {
  //Clear users from state
  clearUsers = () => this.setState({ users: [], loading: false});
 
+//Set Alert
+setAlert = (msg, type) => {
+  this.setState({ alert: { msg, type } });
+};
+
   render() { //lifecicle method required
     const { users, loading } = this.state;
 
@@ -58,6 +64,7 @@ class App extends Component {
           clearUsers={this.clearUsers} 
           // showClear={this.state.users.length > 0 ? true: false}
           showClear={users.length > 0 ? true: false}
+          setAlert={this.setAlert}
           />
           {/* <Users loading={this.state.loading} users={this.state.users}/> */}
           <Users loading={loading} users={users}/>
