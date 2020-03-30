5016c357afa0c0ed3d269d113efd27416d4c0f93
ditasmer <ditasmer@gmail.com>
2020-03-30 16:06:02 +0100

    L23: Use a fragment to do "back to search" button

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
     componentDidMount(){
@@ -33,7 +34,11 @@ static propTypes = {
 
         if(loading) return <Spinner />;
 
-        return <div>{name}</div>;
+        return <Fragment>
+            <Link to='/' className='btn btn-light'>
+                Back To Search
+            </Link>
+        </Fragment>;
     }
 }
 
