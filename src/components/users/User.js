0d28ad280e07a24d70196da287908c3aef7081c0
ditasmer <ditasmer@gmail.com>
2020-03-31 12:31:39 +0100

    L23: Add a button Link to actual Github profiles

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -63,6 +63,9 @@ static propTypes = {
                           <p>{bio}</p>
                       </Fragment>
                   )}
+                  <a href={html_url} className='btn btn-dark my-1'>
+                      Visit Github Profile
+                  </a>
               </div>
             </div>
           </Fragment>
