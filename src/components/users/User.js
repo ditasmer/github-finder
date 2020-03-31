304242ad369246761ddd229de900acea3e557b5f
ditasmer <ditasmer@gmail.com>
2020-03-31 12:38:21 +0100

    L23: Unordered list,: username, company and website

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -17,6 +17,7 @@ static propTypes = {
     render() {
         const {
             name,
+            company,
             avatar_url,
             location,
             bio,
@@ -66,6 +67,25 @@ static propTypes = {
                   <a href={html_url} className='btn btn-dark my-1'>
                       Visit Github Profile
                   </a>
+                  <ul>
+                      <li>
+                          {login && <Fragment>
+                              <strong>Username: </strong> {login}
+                              </Fragment>}
+                      </li>
+
+                      <li>
+                          {company && <Fragment>
+                              <strong>Company: </strong> {company}
+                              </Fragment>}
+                      </li>
+
+                      <li>
+                          {blog && <Fragment>
+                              <strong>Website: </strong> {blog}
+                              </Fragment>}
+                      </li>
+                  </ul>
               </div>
             </div>
           </Fragment>
